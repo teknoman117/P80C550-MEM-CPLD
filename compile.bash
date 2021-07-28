a8bb8525d851cb2aabdd7407465d722546907c3d
Nathaniel R. Lewis <linux.robotdude@gmail.com>
2021-07-28 00:45:51 -0700

    first commit

---
+++ compile.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export WINEPREFIX="$HOME/.wine32"
+export WINEARCH=win32
+
+die() {
+    echo "command failure"
+    exit 1
+}
+
+wine "c:\\Wincupl\\Shared\\cupl.exe" -uabm4kb "c:\\Wincupl\\Shared\\cupl.dl" MEM.pld || die
+
+wine "c:\\Wincupl\\WinCupl\\Fitters\\fit1504.exe" MEM.tt2 -device P1504C44 -preassign ignore -strategy JTAG = off -strategy pin_keep off -strategy output_fast on || die
