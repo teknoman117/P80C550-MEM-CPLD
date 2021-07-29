f725a498e8c27e8a9a93cd421f33f917d19a4cc2
Nathaniel R. Lewis <linux.robotdude@gmail.com>
2021-07-28 21:57:51 -0700

    finish simulation vectors, allow simulate from console

--- compile.bash
+++ compile.bash
@@ -8,12 +8,17 @@ die() {
     exit 1
 }
 
-wine "c:\\Wincupl\\Shared\\cupl.exe" -uabm4kb "c:\\Wincupl\\Shared\\cupl.dl" MEM.pld || die
-
-wine "c:\\Wincupl\\WinCupl\\Fitters\\fit1504.exe" \
-    MEM.tt2 \
-    -device P1504C44 \
-    -preassign keep \
-    -strategy JTAG = off \
-    -strategy pin_keep off \
-    -strategy output_fast on || die
+if [ "${1}" = "-s" ]; then
+    wine "c:\\Wincupl\\Shared\\cupl.exe" \
+        -uabm1s "c:\\Wincupl\\Shared\\cupl.dl" MEM.pld || die
+else
+    wine "c:\\Wincupl\\Shared\\cupl.exe" \
+        -uabm4kb "c:\\Wincupl\\Shared\\cupl.dl" MEM.pld || die
+    wine "c:\\Wincupl\\WinCupl\\Fitters\\fit1504.exe" \
+        MEM.tt2 \
+        -device P1504C44 \
+        -preassign keep \
+        -strategy JTAG = off \
+        -strategy pin_keep off \
+        -strategy output_fast on || die
+fi
