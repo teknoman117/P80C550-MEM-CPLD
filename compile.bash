80f3f0b8232b07c12967d80883b491712a21d1f2
Nathaniel R. Lewis <linux.robotdude@gmail.com>
2021-07-28 20:46:59 -0700

    add simulation, convert line endings

--- compile.bash
+++ compile.bash
@@ -10,4 +10,10 @@ die() {
 
 wine "c:\\Wincupl\\Shared\\cupl.exe" -uabm4kb "c:\\Wincupl\\Shared\\cupl.dl" MEM.pld || die
 
-wine "c:\\Wincupl\\WinCupl\\Fitters\\fit1504.exe" MEM.tt2 -device P1504C44 -preassign keep -strategy JTAG = off -strategy pin_keep off -strategy output_fast on || die
+wine "c:\\Wincupl\\WinCupl\\Fitters\\fit1504.exe" \
+    MEM.tt2 \
+    -device P1504C44 \
+    -preassign keep \
+    -strategy JTAG = off \
+    -strategy pin_keep off \
+    -strategy output_fast on || die
